dfecf81600b4a5d683f213b60df42f2fd9e63611
idk <layer7@kakao.com>
2022-05-12 15:37:55 +0900

    Fix: async 제거 및 값 전달 안되는 현상 해결
    - 현 상황에서 Express 비동기 Error 처리가 어려워 async 제거
    - 이전에 main으로 adminCheck값을 제대로 넘기지 못한 현상 해결

--- 내부-면접용-Web문제/routes/index.js
+++ 내부-면접용-Web문제/routes/index.js
@@ -1,17 +1,20 @@
 var express = require('express');
 const query=require('../db/mysql_query')
 const app = require('../app');
+const { adminCheck } = require('../db/mysql_query');
 var router = express.Router();
 
-router.get('/',async(req,res)=>{
-    console.log(req.session.user)
+router.get('/',(req,res)=>{
+    //console.log(req.session.user)
     if(!req.session.user){
         res.render('main',{isLogin:false})
     }else{
-        res.render('main',{
-            isLogin:true,
-            isAdmin:await query.adminCheck(req.session.user.id)['is_admin'],
-            username:req.session.user.id
+       let adminCheck = query.adminCheck(req.session.user.id).then((adminCheck)=>{
+            res.render('main',{
+                isLogin:true,
+                isAdmin:adminCheck['is_admin'],
+                username:req.session.user.id
+            })
         })
     }
 })
